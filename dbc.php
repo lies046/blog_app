758f0ff3a3916e6226e256e8e06b0fdffdb4b5b0
lies046 <skytree.skysky@gmail.com>
2020-07-19 13:33:26 +0900

    Add PDO

--- dbc.php
+++ dbc.php
@@ -2,6 +2,14 @@
 $dns='mysql:host=localhost;dbname=blog_app;charset=utf8;';
 $user = 'root';
 $pass = '';
-$dbh= new PDO($dns, $user, $pass);
 
-var_dump($dbh);
+try{
+$dbh= new PDO($dns, $user, $pass,[
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+]);
+echo '接続成功';
+$dbh = null;
+}catch(PDOException $e){
+  echo '接続失敗'.$e->getMessage();
+  exit();
+};
