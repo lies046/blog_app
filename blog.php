578f5d36932c004ae551d0570bd3e48418e71b3d
lies046 <skytree.skysky@gmail.com>
2020-07-25 13:45:58 +0900

    Add BlogUpdate.php

--- blog.php
+++ blog.php
@@ -18,10 +18,10 @@ Class Blog extends Dbc
   }
 
   public function blogCreate($blogs){
-    $sql = 'INSERT INTO 
-          blog(title, content, category, publish_status)
+    $sql = "INSERT INTO 
+          $this->table_name(title, content, category, publish_status)
         VALUES
-          (:title, :content, :category, :publish_status)';
+          (:title, :content, :category, :publish_status)";
     $dbh=$this->dbConection();
     $dbh->beginTransaction();
     try{
@@ -39,8 +39,33 @@ Class Blog extends Dbc
       exit($e);
     }
   }
+
+  public function blogUpdate($blogs){
+    $sql = "UPDATE $this->table_name SET
+    title = :title, content = :content, category = :category, publish_status = :publish_status
+    WHERE
+      id = :id";
+    $dbh=$this->dbConection();
+    $dbh->beginTransaction();
+    try{
+    $stmt=$dbh->prepare($sql);
+    $stmt->bindValue(':title',$blogs['title'], PDO::PARAM_STR);
+    $stmt->bindValue(':content',$blogs['content'], PDO::PARAM_STR);
+    $stmt->bindValue(':category',$blogs['category'], PDO::PARAM_INT);
+    $stmt->bindValue(':publish_status',$blogs['publish_status'], PDO::PARAM_INT);
+    $stmt->bindValue(':id',$blogs['id'], PDO::PARAM_INT);
+
+    $stmt->execute();
+    $dbh->commit();
+    echo 'ブログを更新しました。';
+    }catch(PDOException $e){
+    exit($e);
+    $dbh->rollBack();
+    exit($e);
+    }
+      }
   //ブログのバリデーション
-  function blogValidate($blogs){
+  public function blogValidate($blogs){
     if(empty($blogs['title'])){
       exit('タイトルを入力してください');
     }
